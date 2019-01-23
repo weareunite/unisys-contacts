254993b2ac7d28938191d1edb56c6785f6557e90
Vlado Zilka <vlado.zilka@gmail.com>
2019-01-23 15:46:35 +0100

    fix

--- src/Http/Controllers/CountryController.php
+++ src/Http/Controllers/CountryController.php
@@ -63,7 +63,9 @@ class CountryController extends Controller
      */
     public function listForSelect()
     {
-        $object = $this->repository->getListForSelect();
+        $object = Country::orderBy('name', 'asc')
+            ->get(['id', 'name'])
+            ->pluck('name', 'id');
 
         return $this->response->collection($object, CountryForSelectResource::class);
     }
